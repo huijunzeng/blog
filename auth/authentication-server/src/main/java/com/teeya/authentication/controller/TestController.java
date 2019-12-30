ee678d2bf006bb01d18e6437914f9c917b2538a4
huijunzeng <18826233829@163.com>
2019-12-31 00:36:01 +0800

    日志整合

--- auth/authentication-server/src/main/java/com/teeya/authentication/controller/TestController.java
+++ auth/authentication-server/src/main/java/com/teeya/authentication/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.teeya.authentication.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,13 @@ import java.security.Principal;
 @RequestMapping("/test")
 public class TestController {
 
+    private Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @RequestMapping("/hello")
     public String test(){
+        logger.info("ceshi11111");
+        logger.debug("ceshi22222");
+        logger.error("ceshi33333");
         return "hello oauth";
     }
 
