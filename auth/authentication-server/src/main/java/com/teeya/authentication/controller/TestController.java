cc515c3dad660e15a52134f7d0940578ec7eb19a
huijunzeng <18826233829@163.com>
2020-01-02 00:32:11 +0800

    日志整合

--- auth/authentication-server/src/main/java/com/teeya/authentication/controller/TestController.java
+++ auth/authentication-server/src/main/java/com/teeya/authentication/controller/TestController.java
@@ -1,5 +1,9 @@
 package com.teeya.authentication.controller;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +18,28 @@ public class TestController {
 
     private Logger logger = LoggerFactory.getLogger(TestController.class);
 
+    // 对应的 `handleException` 函数需要位于 `ExceptionUtil` 类中，并且必须为 static 函数.
+    // @SentinelResource(value = "test", blockHandler = "handleException", blockHandlerClass = {ExceptionUtil.class})
+    // 若 blockHandler 和 fallback 都进行了配置，则被限流降级而抛出 BlockException 时只会进入 blockHandler 处理逻辑
+    @SentinelResource(value = "test", blockHandler = "exceptionHandler", fallback = "helloFallback")
     @RequestMapping("/hello")
     public String test(){
         logger.info("ceshi11111");
         logger.debug("ceshi22222");
         logger.error("ceshi33333");
         return "hello oauth";
+    }
+
+    // Fallback 函数，函数签名与原函数一致或加一个 Throwable 类型的参数.
+    public String helloFallback(long s) {
+        return String.format("Halooooo %d", s);
+    }
+
+    // Block 异常处理函数，参数最后多一个 BlockException，其余与原函数一致.
+    public String exceptionHandler(long s, BlockException ex) {
+        // Do some log here.
+        ex.printStackTrace();
+        return "Oops, error occurred at " + s;
     }
 
     @GetMapping("/principal")
