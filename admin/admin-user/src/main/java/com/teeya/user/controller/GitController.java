5f2399378e953b60a632a6c6568ed14a7c6b8187
huijunzeng <18826233829@163.com>
2020-03-31 00:16:05 +0800

    去掉引入多余的类

--- admin/admin-user/src/main/java/com/teeya/user/controller/GitController.java
+++ admin/admin-user/src/main/java/com/teeya/user/controller/GitController.java
@@ -1,7 +1,6 @@
 package com.teeya.user.controller;
 
 import com.alibaba.fastjson.JSONObject;
-import com.teeya.user.utils.HttpClientUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
