9cbe6ab15d84c97430ba55e835178457cbabc40a
huijunzeng <18826233829@163.com>
2020-04-13 01:35:59 +0800

    swagger整合

--- admin/admin-user/src/main/java/com/teeya/user/config/SwaggerConfig.java
+++ admin/admin-user/src/main/java/com/teeya/user/config/SwaggerConfig.java
@@ -6,11 +6,18 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.*;
+
 /**
+ * swagger接口文档配置
  * @Author: ZJH
  * @Date: 2019/12/5 10:15
  */
@@ -29,14 +36,51 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.teeya.user"))
                 // 指定路径处理PathSelectors.any()代表所有的路径
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                // 支持的协议
+                .protocols(newHashSet("https", "http"))
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts());
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("API接口文档")
-                .description("mybatis接口")
+                .title("后台用户管理api")
+                .description("后台用户管理接口")
                 .version("1.0")
                 .build();
+    }
+
+    /**
+     * 设置授权信息
+     */
+    private List<ApiKey> securitySchemes() {
+        // 在请求头header添加一个名为Authorization的token
+        return Collections.singletonList(new ApiKey("Authorization", "token", "header"));
+    }
+
+    /**
+     * 授权信息全局应用
+     */
+    private List<SecurityContext> securityContexts() {
+        return Collections.singletonList(
+                SecurityContext.builder()
+                        .securityReferences(
+                                Collections.singletonList(new SecurityReference("Authorization",
+                                        new AuthorizationScope[]{new AuthorizationScope("global", "")}
+                                )))
+                        // 可通过配置正则表达式去排除一些不需要携带token访问的接口 这里不做特殊处理，全部接口访问都需要携带
+                        // 比如.forPaths(PathSelectors.regex("^(?!auth).*$"))  对所有包含"auth"的接口不需要使用securitySchemes
+                        .forPaths(PathSelectors.any())
+                        .build()
+        );
+    }
+
+    @SafeVarargs
+    private final <T> Set<T> newHashSet(T... ts) {
+        if (ts.length > 0) {
+            return new LinkedHashSet<>(Arrays.asList(ts));
+        }
+        return null;
     }
 }
